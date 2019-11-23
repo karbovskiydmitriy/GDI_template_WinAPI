bc321d55d407514b7a2dd66bde8df977a2a8925a
Karbovskiy Dmitriy <karbovskiydmitriy99@gmail.com>
2019-11-23 19:33:22 +0300

    Tiny improvements

--- WinAPI_template/WinAPI_template/Teamplate.c
+++ WinAPI_template/WinAPI_template/Teamplate.c
@@ -1,14 +1,13 @@
 #include "Template.h"
 
 const DWORD windowStyle = WS_OVERLAPPEDWINDOW;
-int prevTime = 0;
 RECT clientRect = {0, 0, 800, 600};
 HDC hdcBack;
 HBITMAP hbmBack;
-const wchar_t *const caption = L"WinAPI program template";
+#define caption L"WinAPI program template"
 #define mainClassName L"MainClass"
 
-WNDCLASSEXW WndClassEx = {sizeof(WNDCLASSEX), 0, (WNDPROC)WindowProc, 0, 0, 0, 0, 0, 0, 0, (LPCWSTR)mainClassName, 0};
+WNDCLASSEXW WndClassEx = {sizeof(WNDCLASSEX), 0, (WNDPROC)WindowProc, 0, 0, (HINSTANCE)0, (HICON)0, (HCURSOR)0, (HBRUSH)0, (LPCWSTR)0, (LPCWSTR)mainClassName, 0};
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -17,15 +16,15 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	RECT R;
 
 	R = clientRect;
-	AdjustWindowRectEx(&R, WS_OVERLAPPED, 0, 0);
+	AdjustWindowRectEx(&R, windowStyle, FALSE, 0);
 	WndClassEx.hInstance = hInstance;
-	WndClassEx.hCursor = LoadCursor(0, IDC_ARROW);
+	WndClassEx.hCursor = LoadCursorW((HINSTANCE)0, IDC_ARROW);
 	RegisterClassEx(&WndClassEx);
-	CreateWindowExW(0, mainClassName, (LPCWSTR)caption, windowStyle, 0, 0, R.right, R.bottom, 0, 0, hInstance, 0);
+	CreateWindowExW(0, mainClassName, caption, windowStyle, 0, 0, R.right, R.bottom, (HWND)0, (HMENU)0, hInstance, (LPVOID)0);
 
-	while (GetMessage(&msg, 0, 0, 0)) {
+	while (GetMessageW(&msg, (HWND)0, 0, 0)) {
 		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		DispatchMessageW(&msg);
 	}
 
 	return 0;
@@ -42,37 +41,40 @@ LRESULT WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 		ShowWindow(hWnd, SW_SHOWNORMAL);
 		UpdateWindow(hWnd);
+		hdc = GetDC((HWND)0);
+		hdcBack = CreateCompatibleDC(hdc);
+		hbmBack = CreateCompatibleBitmap(hdc, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN));
+		SelectObject(hdcBack, hbmBack);
+		ReleaseDC((HWND)0, hdc);
 		return 0;
 	case WM_SIZE:
 		GetClientRect(hWnd, &clientRect);
-		DeleteDC(hdcBack);
-		DeleteObject(hbmBack);
-		hdc = GetDC(0);
-		hdcBack = CreateCompatibleDC(hdc);
-		hbmBack = CreateCompatibleBitmap(hdc, clientRect.right, clientRect.bottom);
-		SelectObject(hdcBack, hbmBack);
-		ReleaseDC(0, hdc);
-		InvalidateRect(hWnd, 0, 0);
 		return 0;
 	case WM_DESTROY:
-		ExitProcess(0);
+		PostQuitMessage(0);
+		return 0;
 	case WM_PAINT:
-		FillRect(hdcBack, &clientRect, 0);
+		Draw();
 		BeginPaint(hWnd, &ps);
 		BitBlt(ps.hdc, 0, 0, clientRect.right, clientRect.bottom, hdcBack, 0, 0, SRCCOPY);
 		EndPaint(hWnd, &ps);
+		InvalidateRect(hWnd, (RECT *)NULL, FALSE);
 		return 0;
 	case WM_KEYDOWN:
 		if (wParam == VK_ESCAPE) {
-			PostQuitMessage (0);
+			PostQuitMessage(0);
 			return 0;
 		}
 	}
+
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 
 }
 
 void Draw()
 {	
+
+	FillRect(hdcBack, &clientRect, (HBRUSH)0);
+	Rectangle(hdcBack, clientRect.left + 40, clientRect.top + 40, clientRect.right - 40, clientRect.bottom - 40);
 
 }
