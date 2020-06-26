e9ddd7d6bb8f2ebb81b64c84ccea1fb1e10ad654
Karbovskiy Dmitriy <karbovskiydmitriy99@gmail.com>
2020-06-26 23:26:55 +0300

    Add files via upload

--- WinAPI_template/WinAPI_template/Teamplate.c
+++ WinAPI_template/WinAPI_template/Teamplate.c
@@ -4,40 +4,41 @@ const DWORD windowStyle = WS_OVERLAPPEDWINDOW;
 RECT clientRect = {0, 0, 800, 600};
 HDC hdcBack;
 HBITMAP hbmBack;
-#define caption L"WinAPI program template"
-#define mainClassName L"MainClass"
 
-WNDCLASSEXW WndClassEx = {sizeof(WNDCLASSEX), 0, (WNDPROC)WindowProc, 0, 0, (HINSTANCE)0, (HICON)0, (HCURSOR)0, (HBRUSH)0, (LPCWSTR)0, (LPCWSTR)mainClassName, 0};
+WNDCLASSEX WndClassEx =
+{
+	sizeof(WNDCLASSEX), 0, (WNDPROC)WindowProc, 0, 0, (HINSTANCE)0,
+	(HICON)0, (HCURSOR)0, (HBRUSH)0, (TCHAR *)0, (TCHAR *)MAIN_CLASS_NAME, 0
+};
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	
 	MSG msg;
 	RECT R;
 
 	R = clientRect;
 	AdjustWindowRectEx(&R, windowStyle, FALSE, 0);
 	WndClassEx.hInstance = hInstance;
-	WndClassEx.hCursor = LoadCursorW((HINSTANCE)0, IDC_ARROW);
+	WndClassEx.hCursor = LoadCursor((HINSTANCE)0, IDC_ARROW);
 	RegisterClassEx(&WndClassEx);
-	CreateWindowExW(0, mainClassName, caption, windowStyle, 0, 0, R.right, R.bottom, (HWND)0, (HMENU)0, hInstance, (LPVOID)0);
+	CreateWindowEx(0, MAIN_CLASS_NAME, CAPTION, windowStyle, 0, 0, R.right, R.bottom, (HWND)0, (HMENU)0, hInstance, (LPVOID)0);
 
-	while (GetMessageW(&msg, (HWND)0, 0, 0)) {
+	while (GetMessageW(&msg, (HWND)0, 0, 0))
+	{
 		TranslateMessage(&msg);
-		DispatchMessageW(&msg);
+		DispatchMessage(&msg);
 	}
 
 	return 0;
-
 }
 
 LRESULT WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-
 	HDC hdc;
 	PAINTSTRUCT ps;
 
-	switch(uMsg) {
+	switch(uMsg)
+	{
 	case WM_CREATE:
 		ShowWindow(hWnd, SW_SHOWNORMAL);
 		UpdateWindow(hWnd);
@@ -61,20 +62,18 @@ LRESULT WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		InvalidateRect(hWnd, (RECT *)NULL, FALSE);
 		return 0;
 	case WM_KEYDOWN:
-		if (wParam == VK_ESCAPE) {
+		if (wParam == VK_ESCAPE)
+		{
 			PostQuitMessage(0);
 			return 0;
 		}
 	}
 
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
-
 }
 
 void Draw()
 {	
-
 	FillRect(hdcBack, &clientRect, (HBRUSH)0);
 	Rectangle(hdcBack, clientRect.left + 40, clientRect.top + 40, clientRect.right - 40, clientRect.bottom - 40);
-
 }
--- WinAPI_template/WinAPI_template/Template.h
+++ WinAPI_template/WinAPI_template/Template.h
@@ -1,5 +1,8 @@
 #include <Windows.h>
 
+#define CAPTION L"WinAPI program template"
+#define MAIN_CLASS_NAME L"MainClass"
+
 INT WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
 LRESULT WindowProc(HWND, UINT, WPARAM, LPARAM);
 void Draw();
