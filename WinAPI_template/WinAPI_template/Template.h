3f7148c37b0d7377597dd157ec58eef9d8618b5c
Karbovskiy Dmitriy <karbovskiydmitriy99@gmail.com>
2020-07-04 15:30:00 +0300

    Delete Template.h

--- WinAPI_template/WinAPI_template/Template.h
+++
@@ -1,8 +0,0 @@
-#include <Windows.h>
-
-#define CAPTION L"WinAPI program template"
-#define MAIN_CLASS_NAME L"MainClass"
-
-INT WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
-LRESULT WindowProc(HWND, UINT, WPARAM, LPARAM);
-void Draw();
